36db81525404cb123a1dee0bd88f7a72c8d60e22
h4773rJL <heatseeker.persa@gmail.com>
2015-10-05 18:44:03 -0500

    identado y espacios

--- namepy.py
+++ namepy.py
@@ -3,24 +3,24 @@
 
 
 from __future__ import print_function
-import os, sys
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 
-def puttextimg ( name, namefile ):
+
+def puttextimg(name, namefile):
     im = Image.open("5.jpg")
     outfile = namefile
 
-    font = ImageFont.truetype("/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans-Bold.ttf",145)
+    font = ImageFont.truetype("/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans-Bold.ttf", 145)
     text = name
-    tcolor = (0 , 0 , 0)
-    text_pos = (515 , 1320)
+    tcolor = (0, 0, 0)
+    text_pos = (515, 1320)
 
     draw = ImageDraw.Draw(im)
     draw.text(text_pos, text, fill=tcolor, font=font)
 
-    im.save(outfile,"JPEG")
+    im.save(outfile, "JPEG")
     return;
 
 
@@ -29,11 +29,11 @@ destiny = 'heatseeker.persa@gmail.com'
 f = open('nombres.csv', 'r')
 
 for line in f:
-	separated=line.split(',')
-	name = separated[0]+" "+separated[1]+" "+separated[2]
-	namefile = name+".JPG"
-	email = separated[3]
-	puttextimg(name, namefile)
-	print(name,email,end='')
+    separated = line.split(',')
+    name = separated[0] + " " + separated[1] + " " + separated[2]
+    namefile = name + ".JPG"
+    email = separated[3]
+    puttextimg(name, namefile)
+    print(name, email, end='')
 
 f.close()
