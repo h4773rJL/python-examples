419213b027cf692f0a70ac682b56542051794f5d
h4773rJL <heatseeker.persa@gmail.com>
2015-10-05 18:31:26 -0500

    leer archivo csv

---
+++ repy.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3
+# -*- coding:utf-8 -*-
+
+destiny = 'heatseeker.persa@gmail.com'
+
+f = open('nombres.csv', 'r')
+
+for line in f:
+	separated=line.split(',')
+	name = separated[0]+" "+separated[1]+" "+separated[2]
+	email = separated[3]
+		if (email
+	print(name,email,end='')
+
+f.close()
