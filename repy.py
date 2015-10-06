3e9489185b645f19a908288df70e7eff8194f843
h4773rJL <heatseeker.persa@gmail.com>
2015-10-05 22:35:57 -0500

    modificado para opensuse 13.2

--- repy.py
+++ repy.py
@@ -6,10 +6,9 @@ destiny = 'heatseeker.persa@gmail.com'
 f = open('nombres.csv', 'r')
 
 for line in f:
-	separated=line.split(',')
-	name = separated[0]+" "+separated[1]+" "+separated[2]
-	email = separated[3]
-		if (email
-	print(name,email,end='')
+    separated = line.split(',')
+    name = separated[0] + " " + separated[1] + " " + separated[2]
+    email = separated[3]
+    print(name, email)
 
 f.close()
