b0da903b7a883eaed96990fb58f5687e6d4a4a3d
h4773rJL <heatseeker.persa@gmail.com>
2015-10-05 22:37:15 -0500

    para opensuse

--- repyim.py
+++ repyim.py
@@ -1,23 +1,22 @@
-#! /usr/bin/python3
+#! /usr/bin/python
 # -*- coding:utf-8 -*-
 
 
 from __future__ import print_function
-import os, sys
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 
-im = Image.open("5.jpg")
+im = Image.open("reconocimiento.jpg")
 outfile = "name.jpg"
 
-font = ImageFont.truetype("/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans-Bold.ttf",145)
+font = ImageFont.truetype("/usr/share/fonts/truetype/DejaVuSans-Bold.ttf",145)
 text = "José Luis Pérez Rendón"
-tcolor = (0,0,0)
-text_pos = (515,1320)
+tcolor = (0, 0, 0)
+text_pos = (715, 1440)
 
 draw = ImageDraw.Draw(im)
 draw.text(text_pos, text, fill=tcolor, font=font)
 
-im.save(outfile,"JPEG")
+im.save(outfile, "JPEG")
 
