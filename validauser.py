f459558ea399bddea5c853b83e92f4758f4c0655
h4773rJL <heatseeker.persa@gmail.com>
2015-10-15 09:48:02 -0500

    final

--- validauser.py
+++ validauser.py
@@ -1,9 +1,6 @@
 #! /usr/bin/python3
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 770336ef9b9d6f0247aea36f20d47fa963fe2dd3
 def ismayor(a):
     if (len(a) < 6):
         print ('El nombre de usuario debe contener al menos 6 caracteres', end='\n')
@@ -25,15 +22,11 @@ def alfanumeric(a):
         print ('No es Alfanumerica', end='\n')
         return False
 
-<<<<<<< HEAD
 print ('Escriba un nombre de usuario de 6 a 12 caracteres alfanumericos', end='\n')
-=======
->>>>>>> 770336ef9b9d6f0247aea36f20d47fa963fe2dd3
 salida = False
 while salida is False:
     try:
         user = str(input('User: '))
-<<<<<<< HEAD
         if (ismenor(user) and ismayor(user) and alfanumeric(user)):
             salida = True
             print (salida, end='\n')
@@ -41,15 +34,3 @@ while salida is False:
             print ('Intente nuevamente...', end='\n')
     except (KeyboardInterrupt, EOFError):
         print ('Escriba un nombre de usuario de 6 a 12 caracteres alfanumericos', end='\n')
-=======
-        #print ('-< ', ismenor(user))
-        #print ('-> ', ismayor(user))
-        #print ('-a1 ', alfanumeric(user))
-        if (ismenor(user) and ismayor(user) and alfanumeric(user)):
-            print ('usuario correcto', end='\n')
-            salida = True
-        else:
-            print ('nuevaente...')
-    except (KeyboardInterrupt, EOFError):
-        print ('Escriba un nombre de usuario de 6 a 12 caracteres alfanumericos')
->>>>>>> 770336ef9b9d6f0247aea36f20d47fa963fe2dd3
