770336ef9b9d6f0247aea36f20d47fa963fe2dd3
h4773rJL <heatseeker.persa@gmail.com>
2015-10-14 23:45:06 -0500

    valida usuarios v0.0001

---
+++ validauser.py
@@ -0,0 +1,37 @@
+#! /usr/bin/python3
+
+def ismayor(a):
+    if (len(a) < 6):
+        print ('El nombre de usuario debe contener al menos 6 caracteres', end='\n')
+        return False
+    return True
+
+
+def ismenor(a):
+    if (len(a) > 12):
+        print ('El nombre de usuario no puede contener mas de 12 caracteres.', end='\n')
+        return False
+    return True
+
+
+def alfanumeric(a):
+    if (a.isalnum()):
+        return True
+    else:
+        print ('No es Alfanumerica', end='\n')
+        return False
+
+salida = False
+while salida is False:
+    try:
+        user = str(input('User: '))
+        #print ('-< ', ismenor(user))
+        #print ('-> ', ismayor(user))
+        #print ('-a1 ', alfanumeric(user))
+        if (ismenor(user) and ismayor(user) and alfanumeric(user)):
+            print ('usuario correcto', end='\n')
+            salida = True
+        else:
+            print ('nuevaente...')
+    except (KeyboardInterrupt, EOFError):
+        print ('Escriba un nombre de usuario de 6 a 12 caracteres alfanumericos')
