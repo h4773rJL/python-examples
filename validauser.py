ec1e398dde36621a0d6b072ed867b5ff1686748f
h4773rJL <heatseeker.persa@gmail.com>
2015-10-15 23:30:27 -0500

    modulo valida usuario

--- validauser.py
+++ validauser.py
@@ -1,14 +1,21 @@
 #! /usr/bin/python3
 
+msg1 = "Escriba un nombre de usuario de 6 a 12 caracteres alfanumericos"
 
-def ismayor(a):
+def ismayor6(a):
+    """
+    Compara si la cadena contiene al menos 6 caracteres
+    """
     if (len(a) < 6):
         print ('El nombre de usuario debe contener al menos 6 caracteres', end='\n')
         return False
     return True
 
 
-def ismenor(a):
+def ismenor12(a):
+    """
+    Compara si la cadena contiene a lo mas 12 caracteres
+    """
     if (len(a) > 12):
         print ('El nombre de usuario no puede contener mas de 12 caracteres.', end='\n')
         return False
@@ -16,21 +23,29 @@ def ismenor(a):
 
 
 def alfanumeric(a):
+    """
+    Compara si la cadena es alfanumerica
+    """
     if (a.isalnum()):
         return True
     else:
         print ('No es Alfanumerica', end='\n')
         return False
 
-print ('Escriba un nombre de usuario de 6 a 12 caracteres alfanumericos', end='\n')
-salida = False
-while salida is False:
-    try:
-        user = str(input('User: '))
-        if (ismenor(user) and ismayor(user) and alfanumeric(user)):
-            salida = True
-            print (salida, end='\n')
-        else:
-            print ('Intente nuevamente...', end='\n')
-    except (KeyboardInterrupt, EOFError):
-        print ('Escriba un nombre de usuario de 6 a 12 caracteres alfanumericos', end='\n')
+
+def vuser():
+    """
+    solicitamos usuario y validamos
+    """
+    salida = False
+    while salida is False:
+        try:
+            print (msg1, end='\n')
+            user = str(input('User: '))
+            if (ismenor12(user) and ismayor6(user) and alfanumeric(user)):
+                salida = True
+            else:
+                print ('Intente nuevamente...', end='\n')
+        except (KeyboardInterrupt, EOFError):
+            print (msg1, end='\n')
+    return salida
